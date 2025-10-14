25f48e21d3f5d5760647f04fd0889cb2107032fd
Alabudih <77508126+Alabudih@users.noreply.github.com>
2025-10-14 23:03:26 +0000

    Solved this section! October 14 2025

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -1,3 +1,21 @@
 // Learning C++ 
 // Exercise 01_02
 // Hello World, by Eduardo Corpeño 
+#include <iostream>
+
+int main(){
+    std::cout << "#######################" << std::endl;
+    std::cout << "#       Hi There !    #" << std::endl;
+    std::cout << "#######################" << std::endl;
+    std::cout << std::endl << std::endl;
+    std::cout << "##############################" << std::endl;
+    std::cout << "# This is my first program!  #" << std::endl;
+    std::cout << "##############################" << std::endl;
+    std::cout << std::endl << std::endl;
+
+
+    
+
+    return(0);
+
+}
